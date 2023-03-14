ba64e7fe5cbd5620e2e8a7f25f10753b099966b7
gavin <gavink1279@hotmail.com>
2023-03-13 21:32:49 -0700

    Added ability to change theme

--- fortifyLogic.py
+++ fortifyLogic.py
@@ -1,4 +1,4 @@
-import sys
+import sys, qdarktheme
 from PyQt6 import QtWidgets, QtGui
 from PyQt6.QtWidgets import QMainWindow, QHeaderView
 from fortifyUi import Ui_MainWindow
@@ -11,6 +11,8 @@ class FortifyLogic(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.resize_table_headers()
+        self.actionDark_Mode.triggered.connect(lambda: self.set_theme(self.actionDark_Mode.text()))
+        self.actionLight_Mode.triggered.connect(lambda: self.set_theme(self.actionLight_Mode.text()))
 
     def resize_table_headers(self):
         """
@@ -24,9 +26,16 @@ class FortifyLogic(QMainWindow, Ui_MainWindow):
         output_table_headers.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         forward_table_headers.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
+    def set_theme(self, mode):
+        if mode == 'Dark Mode':
+            qdarktheme.setup_theme('dark')
+        elif mode == 'Light Mode':
+            qdarktheme.setup_theme('light')
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+    qdarktheme.setup_theme()
     fortifyLogic = FortifyLogic(app)
     fortifyLogic.show()
     sys.exit(app.exec())
