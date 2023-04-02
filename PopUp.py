aed29af1ae9a395cc7ab813a04590ec1229163ad
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 <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>
2023-04-01 21:25:18 -0700

    add rule window finished

--- PopUp.py
+++ PopUp.py
@@ -1,35 +1,79 @@
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
-
+from PyQt6.QtWidgets import QDialog, QFormLayout, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QComboBox, QLineEdit
+from PyQt6.QtCore import Qt
 
 # Can change the filename/class name to something that shows this is adding a rule
 class PopupWindow(QDialog):
     def __init__(self, parent):
         super().__init__(parent)
-        self.setWindowTitle("Popup Window")
-        self.setGeometry(parent.geometry().x() + 50, parent.geometry().y() + 50, 300, 200)
+        self.setWindowTitle("Add Rule")
+        self.setGeometry(parent.geometry().x(), parent.geometry().y(), 800, 150)
 
         # Should probably make all layouts, labels widgets class variables
         # i.e. self.submitBtn = QPushButton
         # Makes it so other classes can modify the widgets
 
         # Creating layout and labels
-        layout = QVBoxLayout()
-        label = QLabel("This is a popup window.")
+        layout = QHBoxLayout()
+        layout2 = QVBoxLayout()
         self.error = QLabel("")
-        button = QPushButton("Close")
-        button2 = QPushButton("aids")
+
+        chain = QComboBox()
+        chain.setPlaceholderText("chain")
+        chain.addItem("INPUT")
+        chain.addItem("OUTPUT")
+        chain.addItem("FORWARD")
+        chain.setFixedWidth(100)
+
+        protocol = QComboBox()
+        protocol.setPlaceholderText("protocol")
+        protocol.addItem("tcp")
+        protocol.addItem("udp")
+        protocol.addItem("icmp")
+        protocol.setFixedWidth(100)
+
+        source = QLineEdit()
+        source.setPlaceholderText("src IP")
+        source.setFixedWidth(120)
+
+        dest = QLineEdit()
+        dest.setPlaceholderText("dst IP")
+        dest.setFixedWidth(120)
+
+        sPort = QLineEdit()
+        sPort.setPlaceholderText("src Port")
+        sPort.setFixedWidth(80)
+
+        dPort = QLineEdit()
+        dPort.setPlaceholderText("dst Port")
+        dPort.setFixedWidth(80)
+
+        state = QComboBox()
+        state.addItem("ESTABLISHED")
+        state.addItem("RELATED")
+        state.addItem("NEW")
+        state.addItem("INVALID")
+        state.setFixedWidth(140)
+
+        button = QPushButton("submit")
+        button.setFixedWidth(120)
 
         # Adding widgets to layout
-        layout.addWidget(label)
+        layout.addWidget(chain)
+        layout.addWidget(protocol)
+        layout.addWidget(source)
+        layout.addWidget(dest)
+        layout.addWidget(sPort)
+        layout.addWidget(dPort)
+        layout.addWidget(state)
         layout.addWidget(self.error)
-        layout.addWidget(button)
-        layout.addWidget(button2)
 
+        layout2.addLayout(layout)
+        layout2.addWidget(button, alignment=Qt.AlignmentFlag.AlignCenter)
         # Connecting buttons to functions
-        button2.clicked.connect(self.show_error)
+        # button2.clicked.connect(self.show_error)
         button.clicked.connect(self.close)
 
-        self.setLayout(layout)
+        self.setLayout(layout2)
 
     def show_error(self):
         self.set_text('Validation Error', self.error)
